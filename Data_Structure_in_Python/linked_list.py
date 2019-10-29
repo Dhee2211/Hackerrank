b4dbd0f5fbf254057601c5787743c2043ed4d88d
Dhee <Dheesf@gmail.com>
2019-10-29 11:38:16 -0700

    Test updated

--- Data_Structure_in_Python/linked_list.py
+++ Data_Structure_in_Python/linked_list.py
@@ -2,18 +2,36 @@
 
 class Node(object):
     def __init__(self, value):
-        self.value = value
-        self.next_node = None
+        self.value = value 
+        self.next = None
     
-    def insert(self, root, value):
-        if root == None: 
-            Node(value)
+    def display(self, root):
+        current = root 
+        while current:
+            print(current.value)
+            current = current.next
     
-        while root != None:
-            root = root.next
+    def insert(self, root, value ):
+        if root == None:
+            root = Node(value)
+
+        elif root.next == None:
+            root.next = Node(value)
+
+        else:
+            self.insert(root.next, value)
+        return root
 
 
-
+if __name__ == "__main__":
+    d = Node(0)
+    head = d.insert(d,2)
+    d = Node(0)
+    head = d.insert(d,1)
+    head = d.insert(d,2)
+    head = d.insert(d,3)
+    head = d.insert(d,4)
+    d.display(head)
 
 
 
